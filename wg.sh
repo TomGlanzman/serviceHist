5235a98a705c59437a8419f820dd2c4a5fe62dfb
TomGlanzman <t.glanzman@gmail.com>
2023-03-01 17:44:11 -0800

    Routine update after a day of trolling ancient history and filling out DESC history...

--- wg.sh
+++ wg.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 #
 # wg.sh - Trivial script to dump out working group conveners
-#  $ bash wg.sh [lastname]
+#  $ bash wg.sh [lastname | memidnum]
 #
 
-lastname=''
-if [ -n "$1" ]
-then
-   lastname=' and LastName like "'$1'" '
-fi
-
 opt=$1
-echo opt="$opt"
 
-is_int () { case $1 in '' | *[!0-9]*) return 1;; esac ; }
-is_int $opt
-echo $?
-echo -------------------
+# Define the location/name of the sqlite3 database file
+dbfile="foo.db"
+cmd="sqlite3 -header -table $dbfile "
 
-if [ ( is_int "$opt" ) -eq 0 ]
+# Check of opt is a number (==> memidnum) or not (==> lastname) or <null>
+if [[ "$opt" =~ ^[0-9]+$ ]]
 then
-    opt=' and MEMNUMID='$opt' '
+    opt=' and MEMIDNUM='$opt' '
 elif [ -n "$opt" ]
 then
     opt=' and LastName like "'$opt'" '
@@ -28,16 +21,19 @@ else
     opt=''
 fi
 
-echo opt=$opt
+echo "          ==================================================================="
+echo "          =========[co-]conveners============================================"
+echo "          ==================================================================="
 
+if [ -z "$opt" ]
+then
+    echo "=====by name====="
+    sql1="select FirstName,LastName,MEMIDNUM,GroupName,RoleStart,RoleEnd from DevSummary where RoleID in (9) ${opt} order by LastName,RoleStart;"
+    $cmd "${sql1}"
+    echo
+fi
 
+echo "=====by group====="
+sql2="select FirstName,LastName,MEMIDNUM,GroupName,RoleStart,RoleEnd from DevSummary where RoleID in (9) ${opt} order by GroupID,RoleStart;"
+$cmd "${sql2}"
 
-
-
-
-
-#sql="select FirstName,LastName,MEMIDNUM,GroupName,RoleStart,RoleEnd from DevSummary where RoleID in (9) ${lastname} order by LastName,RoleStart;"
-
-#sqlite3 -header -table -echo foo.db "${sql}"
-
-#sqlite3 -header -table foo.db 'select * from DevSummary where RoleID in (9) order by LastName,RoleStart limit 10;select * from DevSummary where RoleID in (9) order by GroupID,RoleStart limit 10;'
