06075fdf0fcafbe5b085fb7d9b2327baf6e2a870
TomGlanzman <t.glanzman@gmail.com>
2023-07-06 10:15:31 -0700

    PubBoard rotation (and report fix)

--- pub.sql
+++ pub.sql
@@ -3,10 +3,13 @@
 .print Complete history of Publication organization
 .print
 .print Publication Committee (policy body)
-select * from DevSummary where GroupID=17 order by RoleStart,RoleID;
+select * from HistSummary where GroupID=17 order by RoleStart,RoleID;
 .print
 .print Publication Board (working body)
-select * from DevSummary where GroupID=6 order by RoleStart,RoleID;
+select * from HistSummary where GroupID=6 order by RoleStart,RoleID;
+.print
+.print *CURRENT* PubBoard
+select * from HistSummary where GroupID=6 and RoleEnd = '' order by RoleStart,RoleID;
 
 
 
