97902051f217e8d7732b873740b6365d1660fb3c
TomGlanzman <t.glanzman@gmail.com>
2023-07-06 09:45:59 -0700

    Advisory Board rotation (and fix to its .sql report file)

--- advisory.sql
+++ advisory.sql
@@ -3,6 +3,6 @@
 .print Complete history of DESC Executive & Advisory Boards
 .print
 
-select FirstName,LastName,MEMIDNUM,GroupName,Role,RoleID,RoleStart,RoleEnd from DevSummary where GroupID in (2,3) order by RoleStart,Role,LastName;
+select FirstName,LastName,MEMIDNUM,GroupName,Role,RoleID,RoleStart,RoleEnd from HistSummary where GroupID in (2,3) order by RoleStart,Role,LastName;
 
 .trace off
