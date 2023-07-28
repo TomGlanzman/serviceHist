7240fb4040f43c7b4bff403d5ea76eec654e45ec
TomGlanzman <t.glanzman@gmail.com>
2023-07-27 17:09:33 -0700

    update the sorting (by startdate) for the groups report

--- groups.sql
+++ groups.sql
@@ -5,18 +5,18 @@ select "All of the DESC-defined groups as of ",date();
 
 .read tFormat.sql
 .print Management entities
-select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID < 100 order by GroupID;
+select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID < 100 order by GROUPSTART;
 
 .print
 .print Working Groups
-select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID between 100 and 199 order by GroupID;
+select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID between 100 and 199 order by GroupStart;
 
 .print
 .print Topical Teams
-select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID between 200 and 299 order by GroupID;
+select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID between 200 and 299 order by GroupStart;
 
 .print
 .print Task Forces
-select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID between 300 and 399 order by GroupID;
+select GROUPNAME,GROUPABBRV,GROUPSTART,GROUPEND,NOTES from desc_org_groups where GroupID between 300 and 399 order by GroupStart;
 
 --.trace off
