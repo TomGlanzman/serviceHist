477c1d3daa0b2c29ed3f819e3ca1f1e127e4a25d
TomGlanzman <t.glanzman@gmail.com>
2023-08-31 15:30:27 -0700

    Fixed sql bug in 'or' clause; added NOTES column; improved column order and sorting

--- chairs.sql
+++ chairs.sql
@@ -6,25 +6,25 @@ select "Current and Past Chairs and Membership of Committees, Boards, & Bureaus 
 .read tFormat.sql
 .print NOTE: some groups have not designated a chair/manager.
 .print Complete history of committee chairs (sorted by name)
-select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd from HistSummary
+select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd,Notes from HistSummary
        where RoleID in (5,6,10,15)
-       and (GroupID between 2 and 19) or (GroupID between 50 and 100)
+       and ((GroupID between 2 and 19) or (GroupID between 50 and 100))
        order by LastName,RoleStart;
 
 .print ================================================================================================
 .print
-.print Complete history of committee chairs (sorted by group)
-select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd from HistSummary
+.print Complete history of committee chairs (sorted by group and time)
+select GroupName,FirstName,LastName,Role,RoleStart,RoleEnd,Notes from HistSummary
        where RoleID in (5,6,10,15)
-       and (GroupID between 2 and 19) or (GroupID between 50 and 100)
-       order by GroupID,RoleStart,LastName;
+       and ((GroupID between 2 and 19) or (GroupID between 50 and 100))
+       order by GroupName,RoleStart,LastName;
 
 --
 .print ================================================================================================
 .print
-.print Complete history of full committee membership (sorted by group)
-select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd from HistSummary
-       where GroupID between 2 and 19
-       or GroupID between 50 and 100
-       order by GroupID,RoleStart,LastName;
+.print Complete history of full committee membership (sorted by group and time)
+select GroupName,FirstName,LastName,Role,RoleStart,RoleEnd,Notes from HistSummary
+       where (GroupID between 2 and 19)
+       or (GroupID between 50 and 100)
+       order by GroupName,RoleStart,LastName;
 
