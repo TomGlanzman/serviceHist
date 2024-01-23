2723363f1078c420cd0bcd01cd772a374c33b1ed
TomGlanzman <t.glanzman@gmail.com>
2024-01-23 09:45:58 -0800

    Improved/corrected comments in both files

--- AllNames.sql
+++ AllNames.sql
@@ -1,6 +1,11 @@
 /* AllNames.sql - This is a DbVisualizer Pro specific SQL snippet which produces a file containing
    		  a list of all names and IDs from shared DESC user database (config-prod). 
-                  Use the createAllNames.sh script to run this query.
+                  Use the makeAllNames.sh script to run this query.
+
+		  Success assumes that you already have a DbVisualizer
+		  profile established that contains the credentials to
+		  access the DESC Oracle database.
+
 */
 @export on;
 @export set filename="./AllNames.txt" appendfile="clear" CsvIncludeColumnHeader=false CsvColumnDelimiter=",";
