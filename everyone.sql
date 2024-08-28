6717bd0cd762b9e33ef4e30dc4e035586c799f63
TomGlanzman <t.glanzman@gmail.com>
2024-08-28 09:53:43 -0700

    ordering now by LastName,FirstName because we now have multiple individuals with the same LastName

--- everyone.sql
+++ everyone.sql
@@ -4,7 +4,7 @@
 select "Complete history of DESC service records (sorted by person's name) as of ",date(datetime(),'localtime');
 
 .read tFormat.sql
-select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd from HistSummary order by LastName,RoleStart;
+select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd from HistSummary order by LastName,FirstName,RoleStart;
 
 .print
 .print Total number of entries in table:
