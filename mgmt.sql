8fe09a7d63d4a1f32ebb58f8212866340746af1d
TomGlanzman <t.glanzman@gmail.com>
2025-09-02 14:37:52 -0700

    Admin transition update along with various other groups.  Update mgmt.sql to include the new 8th admin

--- mgmt.sql
+++ mgmt.sql
@@ -43,7 +43,14 @@ select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd,Notes from HistSummar
 .print
 .print Seventh Administration
 select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd,Notes from HistSummary
-       where RoleStart > '2023-06-30' and RoleStart < '2025-07-01' and GroupID in (20,21,22,23,24,25,26)
+       where RoleStart > '2023-06-30' and RoleStart < '2025-10-01' and GroupID in (20,21,22,23,24,25,26)
+       order by GroupID,RoleID ;
+
+
+.print
+.print Eighth Administration (Dates of office now extend from 1-Oct to 30-Sep two years hence)
+select FirstName,LastName,Role,GroupName,RoleStart,RoleEnd,Notes from HistSummary
+       where RoleStart > '2025-09-30' and RoleStart < '2027-10-01' and GroupID in (20,21,22,23,24,25,26)
        order by GroupID,RoleID ;
 
 
