6734562fc74d7c0e3e891eebb3b38d84b384ff09
TomGlanzman <t.glanzman@gmail.com>
2026-02-11 13:14:21 -0800

    add current SOC/LOC; update AllNames generation

--- makeAllNames.sh
+++ makeAllNames.sh
@@ -1,9 +1,13 @@
 # makeAllNames.sh - Produce a list of member names and ID numbers from the DESC OracleDB.
-#               This script should be sourced ($ . ./makeAllNames.sh).
-#               Configured to work from descops account on S3DF at SLAC
-#               using Tom's dbvis profile.
-#               Output is a text file:  AllNames.txt
-dbviscmd -prefsdir ~descops/tom.dbvis -connection "SRS Config" -sqlfile ./AllNames.sql
 #
-# A slight different version of this command formerly worked on comet2 with an older dbvis:
+#               This script should be sourced ($ . ./makeAllNames.sh)
+#               from within the serviceHist directory.
+#
+#               Configured to work from descops account on S3DF at
+#               SLAC using Tom's dbvis profile.  Output is a text
+#               file: AllNames.txt
+#
+dbviscmd -prefsdir ~descops/tom.dbvis -connection "sca config" -sqlfile ./AllNames.sql
+#
+# A slightly different version of this command formerly worked on comet2 with an older dbvis:
 # $ /opt/DbVisualizer/dbviscmd.sh -connection "config-prod" -sqlfile ./AllNames.sql
